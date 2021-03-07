ff15fc21f478b79d5b09bcd117a1773b91040dde
alex.zhang <gzane24@hotmail.com>
2021-03-07 10:37:42 +0800

    fix: module

--- src/modules/home/HomeModule.ts
+++ src/modules/home/HomeModule.ts
@@ -3,6 +3,15 @@ import { GetLogger, ILogger, Provider, Value } from '@augejs/core';
 
 @Provider()
 export class HomeModule {
+  /**
+   * @api {post} /tool/test Tool
+   * @apiGroup tool
+   *
+   * @apiParam {String} merchantId="106000000000021" merchantId
+   * @apiParam {String} merchantKey="xxxxxx" merchantKey
+   * @apiParam {String="MD5","SHA"} signType="MD5" signType
+   * @apiSampleRequest /test/toll
+   */
   @RequestMapping.Get('/ping')
   async ping(): Promise<string> {
     return 'pong';
